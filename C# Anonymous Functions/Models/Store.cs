a0315116a765d08a25cb27976d543b42c3757f61
giorgi7 <173785707+GGiorgadze7@users.noreply.github.com>
2026-09-10 09:00:01 +0400

    Add project files.

---
+++ C# Anonymous Functions/Models/Store.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Anonymous_Functions.Models
+{
+    public class Store : IDisposable
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public StoreType Type { get; set; }
+
+        private bool disposed = false;
+
+        public Store(string name, string adress, StoreType type)
+        {
+            Name = name;
+            Adress = adress;
+            Type = type;
+
+
+            Console.WriteLine($"magazia gaixsna: {name}");
+            
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"saxeli: {Name}, misamarti: {Adress}, magaziis tipi: {Type}");
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                Console.WriteLine($"magazia daixura: {Name}");
+                disposed = true;
+            }
+        }
+
+        ~Store()
+        {
+            Dispose();
+        }
+
+
+
+    }
+}
---
+++ C# Anonymous Functions/Models/StoreType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Anonymous_Functions.Models
+{
+    public  enum StoreType
+    {
+        Grocery,
+        Electronics, 
+        Clothing,   
+        Shoes
+    }
+}
